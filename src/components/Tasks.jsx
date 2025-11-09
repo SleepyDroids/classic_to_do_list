28329f61c54f7d3c71f6b33f4144187876989d09
SleepyDroids <jacquelinelwrites@gmail.com>
2025-11-09 01:28:31 -0500

    added labels for checkbox inputs for accessibility

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,21 +1,32 @@
 import { useState } from "react";
 
-export default function Tasks({ tasks, handleOnChange, deleteTask, onEditClick }) {
-
+export default function Tasks({
+  tasks,
+  handleOnChange,
+  deleteTask,
+  onEditClick,
+}) {
   return (
     <ul className="tasks" role="list">
       {tasks.map((t) => {
         return (
           <li key={t.id}>
-            <input
-              type="checkbox"
-              id={t.id}
-              name="task"
-              checked={t.completed}
-              onChange={() => handleOnChange(t.id)}
-            />
-            <span className={t.completed ? "completed" : null}>{t.text}</span>
-            <button className="edit-btn" onClick={() => onEditClick(t.id, t.text)}>🖊️</button>
+            <label>
+              <input
+                type="checkbox"
+                id={t.id}
+                name="task"
+                checked={t.completed}
+                onChange={() => handleOnChange(t.id)}
+              />
+              <span className={t.completed ? "completed" : null}>{t.text}</span>
+            </label>
+            <button
+              className="edit-btn"
+              onClick={() => onEditClick(t.id, t.text)}
+            >
+              🖊️
+            </button>
             {/*  when this specific button is clicked, call deleteTask and pass in the id for this particular task (t.id) */}
             <button
               className="del-btn"
